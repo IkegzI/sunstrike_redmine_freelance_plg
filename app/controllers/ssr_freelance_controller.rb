d2fdad9927ce9947a8fa256f7d2e6da1e69cdb66
IkegzI <kegz@mail.ru>
2020-02-18 13:31:13 +0800

    fix bugs 180220/2

--- app/controllers/ssr_freelance_controller.rb
+++ app/controllers/ssr_freelance_controller.rb
@@ -38,15 +38,15 @@ class SsrFreelanceController < ApplicationController
 
       role_ids_custom = SsrFreelanceSetting.all.map { |item| item.role_id }.compact
       check = (role_user_ids.map { |item| 1 if role_ids_custom.include?(item) }).compact.pop || 2
-      # if issue
-      #   if check == 2
-      #     if project.issues.find(issue).custom_values.find_by(custom_field_id: Setting.plugin_sunstrike_redmine_freelance_plg['sunstrike_freelance_field_id'].to_i).value.to_i == 1
-      #       check = 3
-      #     else
-      #       check = 2
-      #     end
-      #   end
-      # end
+      if issue
+        if check == 2
+          if project.issues.find(issue).custom_values.find_by(custom_field_id: Setting.plugin_sunstrike_redmine_freelance_plg['sunstrike_freelance_field_id'].to_i).value.to_i == 1
+            check = 3
+          else
+            check = 2
+          end
+        end
+      end
     end
     respond_to do |format|
       format.html {
